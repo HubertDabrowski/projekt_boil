e184781b0ed05db0c8265fd5ce0468c741fb1be5
mfus <fus.maciek@gmail.com>
2023-03-12 23:45:04 +0100

    v.1

---
+++ backend/src/main/java/pl/fus/backend/Component.java
@@ -0,0 +1,14 @@
+package pl.fus.backend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Component {
+//
+//    @GetMapping("/")
+//    ResponseEntity<?> homePage
+}
