b55defce4d822d6e09d6ce586f1fb8c5f504466e
hdabrowski@student.agh.edu.pl <dabro.hubert@gmail.com>
2023-03-26 19:56:10 +0200

    style w tailwind

--- front/src/CPMForms.js
+++ front/src/CPMForms.js
@@ -17,12 +17,16 @@ const CPMForms = () => {
     }
   };
   return (
-    <div>
-      <button onClick={plus}>+</button>
-      {amount}
-      <button onClick={minus}>-</button>
-      {forms}
-      <button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="pt-10 pb-10 flex flex-row">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={plus}>+</button>
+        <span className="text-lg pl-5 pr-5">{amount}</span>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={minus}>-</button>
+      </div>
+      <div className="ml-10 mr-10 pb-10 grid grid-rows-3 grid-flow-col gap-4">
+        {forms}
+      </div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         <Link to="/cpm/results">Oblicz</Link>
       </button>
     </div>
--- front/src/Home.js
+++ front/src/Home.js
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (<div>
-    <button>
-      <Link to='/cpm/forms'>CPM</Link>
-    </button>
-    <button>
-      <Link to='/pos/forms'>Pośrednik</Link>
-    </button>
-  </div>);
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white">Wybierz metodę</p>
+      <div className="h-40 grid grid-cols-2 gap-4 content-evenly ...">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          <Link to="/cpm/forms">CPM</Link>
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          <Link to="/pos/forms">Pośrednik</Link>
+        </button>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Home;
