ba56e1498da21db04215328b41871b1e58eaec1c
mfus <fus.maciek@gmail.com>
2023-04-14 17:06:42 +0200

    cors v2

--- backend/src/main/java/pl/fus/backend/Component.java
+++ backend/src/main/java/pl/fus/backend/Component.java
@@ -15,7 +15,7 @@ public class Component {
         this.cpmService = cpmService;
     }
 
-    @CrossOrigin("http://localhost:3000/")
+    @CrossOrigin(origins = {"http://localhost:3000/","http:://localhost:8080"})
     @RequestMapping(value = "/cpm", method = RequestMethod.POST, consumes = "application/json")
     public ResponseEntity<ResponseTask[]> returnListOfTasks(@RequestBody TaskDTO[] tasks) {
         ResponseTask[] responseTasks = cpmService.calculate(tasks);
